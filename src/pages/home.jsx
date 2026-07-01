4074840e78382a10a5e0a6da8df76cee9e4ca0f9
Ayo100x <ayomideadelooye@gmail.com>
2026-07-01 11:50:32 +0100

    HomePage

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,19 +1,52 @@
-import { useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import Header from "../components/header";
 import PostComposer from "../components/postComposer";
 import PostCard from "../components/postCard";
+import { get_following_post } from "../../services/post.services";
+import { usePostStore } from "../stores/post.store";
+import CommentComposer from "../components/commentComposer";
 
-const Home = () => {
+const Home = ({ scrollRef }) => {
+  const posts = usePostStore((state) => state.posts);
+
+  useEffect(() => {
+    const saved = sessionStorage.getItem("home-scroll");
+
+    if (!saved) return;
+
+    const el = scrollRef.current;
+
+    if (!el) return;
+
+    requestAnimationFrame(() => {
+      el.scrollTop = Number(saved);
+    });
+  }, [posts]);
+
+  useEffect(() => {
+    const el = scrollRef.current;
+
+    const saveScroll = () => {
+      sessionStorage.setItem("home-scroll", el.scrollTop);
+    };
+
+    el?.addEventListener("scroll", saveScroll);
+
+    return () => {
+      el?.removeEventListener("scroll", saveScroll);
+    };
+  }, []);
+
   return (
-    <div className="h-full flex flex-col bg-black text-white border-r border-white/10">
+    <div className="h-screen flex flex-col bg-black text-white border-r border-white/10">
       <Header />
-      <div className="overflow-y-auto hide-scrollbar ">
+      <div ref={scrollRef} className="flex-1 overflow-y-auto hide-scrollbar ">
         <PostComposer />
-        {[1, 2, 3, 4, 5].map((post, i) => {
-          const hasQuote = i % 2 === 0;
-
-          return <PostCard post={hasQuote ? {quote: { post_id: "ben" }} : {}} key={i} />;
-        })}
+        {posts
+          .filter((post) => post.replyId === null)
+          .map((post) => (
+            <PostCard key={post.postId} post={post} />
+          ))}
       </div>
     </div>
   );
