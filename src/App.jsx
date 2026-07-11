3057d82e5292ac584902d8f0e1cc327dc5fe5be4
Ayo100x <ayomideadelooye@gmail.com>
2026-07-11 18:16:51 +0100

    App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,47 +1,122 @@
-import { Route, Routes } from "react-router-dom";
+import { Route, Routes, useLocation } from "react-router-dom";
 import SideBar from "./components/sideBar";
 import Home from "./pages/home";
 import Profile from "./pages/profile";
 import Notifications from "./pages/notifications";
 import Bookmarks from "./pages/bookmarks";
 import Explore from "./pages/explore";
-import Chat from "./pages/chat";
+import ChatPage from "./pages/chatPage";
 import RightPanel from "./components/rightPanel";
 import PostPage from "./pages/postPage";
 import { MessageCircle } from "lucide-react";
 import FloatingButtons from "./components/floatingButtons";
 import AiMiniChat from "./components/aiMiniChat";
 import ChatMini from "./components/chatMini";
+import { useRef } from "react";
+import { useCommentComposerStore } from "./stores/useCommentComposerStore";
+import CommentComposer from "./components/commentComposer";
+import { usePostStore } from "./stores/post.store";
+import QuoteComposer from "./components/quoteComposer";
+import { useQuoteComposerStore } from "./stores/useQuoteComposerStore";
+import { useNewMessageModalStore } from "./stores/useNewMessageModalStore";
+import NewMessageModal from "./components/newMessageModal";
 
 function App() {
+  const scrollRef = useRef(null);
+  const showCommentComposer = useCommentComposerStore(
+    (state) => state.showCommentComposer,
+  );
+  const closeCommentComposer = useCommentComposerStore(
+    (state) => state.closeCommentComposer,
+  );
+  const activeReplyPost = useCommentComposerStore((state) => state.activePost);
+
+  const showQuoteComposer = useQuoteComposerStore(
+    (state) => state.showQuoteComposer,
+  );
+  const closeQuoteComposer = useQuoteComposerStore(
+    (state) => state.closeQuoteComposer,
+  );
+  const activeQuotePost = useQuoteComposerStore((state) => state.activePost);
+
+  const location = useLocation();
+
+  const isChatPage = location.pathname.startsWith("/chatPage");
+
+  const showNewMessageModal = useNewMessageModalStore(
+    (state) => state.showNewMessageModal,
+  );
+
+  const closeNewMessageModal = useNewMessageModalStore(
+    (state) => state.closeNewMessageModal,
+  );
+
   return (
     <div className="bg-black relative">
-      <div className="flex h-screen max-w-fit mx-auto  overflow-hidden">
-        <SideBar />
+      <div
+        className={`flex h-screen overflow-hidden ${
+          isChatPage
+            ? "w-full max-w-[1800px] mx-auto px-25"
+            : "max-w-fit mx-auto"
+        }`}
+      >
+        <SideBar scrollRef={scrollRef} isChatPage={isChatPage} />
 
         {/* Main Content Area */}
         <div className="flex flex-1 min-w-0">
           {/* Page */}
-          <div className="w-150  text-white border-r border-white/10">
-            <Routes >
-              <Route path="/" element={<Home />} />
+          <div
+            className={`text-white border-r border-white/10 ${
+              isChatPage ? "flex-1" : "w-150"
+            }`}
+          >
+            <Routes>
+              <Route path="/" element={<Home scrollRef={scrollRef} />} />
               <Route path="/profile" element={<Profile />} />
               <Route path="/notifications" element={<Notifications />} />
               <Route path="/bookmarks" element={<Bookmarks />} />
               <Route path="/explore" element={<Explore />} />
-              <Route path="/chat" element={<Chat />} />
-              <Route path="/post/:id" element={<PostPage/>} />
+              <Route path="/chatPage" element={<ChatPage />} />
+              <Route path="/post/:id" element={<PostPage />} />
             </Routes>
           </div>
 
           {/* Right Panel */}
-          <RightPanel />
+          {!isChatPage && <RightPanel />}
         </div>
       </div>
 
+      {!isChatPage && <FloatingButtons />}
 
-      <FloatingButtons />
-      
+      {showCommentComposer && activeReplyPost && (
+        <div className="fixed inset-0 z-50 bg-black/40 flex justify-center py-12">
+          <CommentComposer post={activeReplyPost} />
+        </div>
+      )}
+      {showQuoteComposer && activeQuotePost && (
+        <div className="fixed inset-0 z-50 bg-black/40 flex justify-center py-12">
+          <QuoteComposer post={activeQuotePost} />
+        </div>
+      )}
+
+      {showNewMessageModal && (
+        <div>
+          {/* transparent overlay */}
+          <div
+            onClick={closeNewMessageModal}
+            className=" bg-transparent overflow-hidden h-full fixed inset-0 z-40"
+          />
+
+          {/* NewMessageModal */}
+
+          <div
+            className="absolute inset-0 z-50 flex justify-center"
+          >
+             <NewMessageModal/>
+          </div>
+
+        </div>
+      )}
     </div>
   );
 }
