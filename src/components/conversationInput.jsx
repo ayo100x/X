3f7bfa7c6f39444776bf17614f412eaa31d8ef13
Ayo100x <ayomideadelooye@gmail.com>
2026-07-16 00:11:12 +0100

    CI

--- src/components/conversationInput.jsx
+++ src/components/conversationInput.jsx
@@ -1,4 +1,4 @@
-import { AudioLines, ImagePlay, Plus, Sticker } from "lucide-react";
+import { ArrowUp, AudioLines, ImagePlay, Plus, Sticker } from "lucide-react";
 import { useEffect, useRef, useState } from "react";
 
 const ConversationInput = () => {
@@ -19,18 +19,18 @@ const ConversationInput = () => {
   }, [text]);
 
   return (
-    <div className="bg-transparent w-full flex items-end gap-2 p-4">
+    <div className="bg-transparent w-full flex items-end gap-2 pb-4 px-4">
       <div className="flex gap-2">
         {/* + button */}
-        <button className="flex items-center justify-center rounded-full shrink-0 bg-white/10 size-12">
+        <button className="flex items-center justify-center rounded-full shrink-0 bg-white/10 size-12 hover:hover:bg-white/20 duration-200 transition-colors">
           <Plus size={20} className="text-white" />
         </button>
         {/* GIF button */}
-        <button className="flex items-center justify-center rounded-full shrink-0 bg-white/10 size-12">
+        <button className="flex items-center justify-center rounded-full shrink-0 bg-white/10 hover:hover:bg-white/20 duration-200 size-12">
           <ImagePlay size={20} className="text-white" />
         </button>
         {/* Sticker button*/}
-        <button className="flex items-center justify-center rounded-full shrink-0 bg-white/10 size-12">
+        <button className="flex items-center justify-center rounded-full shrink-0 bg-white/10 hover:hover:bg-white/20 duration-200 size-12">
           <Sticker size={20} className="text-white" />
         </button>
       </div>
@@ -42,11 +42,17 @@ const ConversationInput = () => {
           onChange={(e) => setText(e.target.value)}
           rows={1}
           placeholder="Message"
-          className="flex-1 bg-transparent resize-none outline-none text-white placeholder:text-white/70 leading-6 max-h-[200px] px-2 py-1 custom-scrollbar"
+          className="flex-1 bg-transparent resize-none outline-none text-white placeholder:text-white/70 leading-6 max-h-50 px-2 py-1 custom-scrollbar"
         />
 
-        <button className="ml-2 flex items-center justify-center size-8 rounded-full bg-white/30 shrink-0">
-          <AudioLines size={20} className="text-white" />
+        <button
+          className={`ml-2 flex items-center justify-center size-8 rounded-full duration-200 shrink-0 ${!text ? "bg-white/20 hover:hover:bg-white/30" : "bg-blue-400 hover:hover:bg-blue-500"} `}
+        >
+          {!text ? (
+            <AudioLines size={20} className="text-white" />
+          ) : (
+            <ArrowUp size={20} className="text-white" />
+          )}
         </button>
       </div>
     </div>
