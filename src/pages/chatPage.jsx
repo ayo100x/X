185907f69a022272d9ace2baa2c362be67efe323
Ayo100x <ayomideadelooye@gmail.com>
2026-07-15 16:08:18 +0100

    chatPage

--- src/pages/chatPage.jsx
+++ src/pages/chatPage.jsx
@@ -6,13 +6,13 @@ import ConversationScreen from "../components/conversationScreen";
 const ChatPage = () => {
   return (
     <div className="flex h-screen ">
-      <div className="w-[500px] border-r border-white/10">
+      <div className="w-[500px] ">
         <ChatList />
       </div>
 
-      <div className="flex-1">
-        <NewChatScreen />
-        {/* <ConversationScreen /> */}
+      <div className="flex-1 w-full">
+        {/* <NewChatScreen /> */}
+        <ConversationScreen />
       </div>
     </div>
 
