25f7d1c12ea876c10ed3333b511a388c7acb54d9
Ayo100x <ayomideadelooye@gmail.com>
2026-07-21 13:35:44 +0100

    message bubble

--- src/components/messageBubble.jsx
+++ src/components/messageBubble.jsx
@@ -1,18 +1,27 @@
 const MessageBubble = ({ message }) => {
   const isCurrentUser = message.senderId === 0;
+  // console.log(message.media)
+  
   return (
     <div
       className={`flex m-1 ${isCurrentUser ? "justify-end" : "justify-start"}`}
     >
-      <div
-        className={`max-w-[70%] rounded-full px-4 py-2 ${
-          isCurrentUser ? "bg-[#1d9bf0]" : "bg-white/10"
-        }`}
-      >
-        <span className="text-[15px] text-white wrap-break-word">
-          {message.text}
-        </span>
-      </div>
+      {message.media && (
+        <div className="max-w-[70%] rounded-full">
+          <img src={message.media} alt="" className="w-100 h-fit rounded-3xl border-white/10 border" />
+        </div>
+      )}
+      {message.text && (
+        <div
+          className={`max-w-[70%] rounded-full px-4 py-2 ${
+            isCurrentUser ? "bg-[#1d9bf0]" : "bg-white/10"
+          }`}
+        >
+          <span className="text-[15px] text-white wrap-break-word">
+            {message.text}
+          </span>
+        </div>
+      )}
     </div>
   );
 };
