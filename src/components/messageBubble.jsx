8541abba703c6608d64e6d2e7aceb81987c10a80
Ayo100x <ayomideadelooye@gmail.com>
2026-07-25 02:26:07 +0100

    fixed more button to toggle and displayed message with media

--- src/components/messageBubble.jsx
+++ src/components/messageBubble.jsx
@@ -14,6 +14,14 @@ const MessageBubble = ({
     openedContextMenuId === message.messageId;
   const showContextMenu = openedContextMenuId === message.messageId;
 
+  const toggleMoreButton = () => {
+    if (showContextMenu) {
+      setOpenedContextMenuId(null)
+    } else {
+      setOpenedContextMenuId(message.messageId)
+    }
+  }
+
   return (
     <div
       className={`flex items-end group
@@ -26,7 +34,7 @@ const MessageBubble = ({
             <button
               onClick={(e) => {
                 e.stopPropagation();
-                setOpenedContextMenuId(message.messageId);
+                toggleMoreButton();
               }}
               className="size-8 rounded-full flex items-center justify-center hover:bg-white/10"
             >
@@ -44,15 +52,25 @@ const MessageBubble = ({
         </div>
       )}
 
-      <div
-        className={`w-fit max-w-[70%] py-2 px-4 rounded-[20px] ${
-          isCurrentUser ? "bg-[#1d9bf0] " : "bg-white/10"
-        }`}
-      >
-        <span className="text-[15px] leading-5 text-white wrap-break-word whitespace-pre-wrap shrink-0">
-          {message.text}
-        </span>
-      </div>
+      {message.text && (
+        <div
+          className={`w-fit max-w-[70%] py-2 px-4 rounded-[20px] ${
+            isCurrentUser ? "bg-[#1d9bf0] " : "bg-white/10"
+          }`}
+        >
+          <span className="text-[15px] leading-5 text-white wrap-break-word whitespace-pre-wrap shrink-0">
+            {message.text}
+          </span>
+        </div>
+      )}
+
+      {message.media && (
+        <img
+          src={message.media}
+          alt=""
+          className="w-90 rounded-3xl border border-white/10"
+        />
+      )}
 
       {!isCurrentUser && showActionButton && (
         <div className="flex items-center gap-1 mb-2.5">
@@ -63,7 +81,7 @@ const MessageBubble = ({
             <button
               onClick={(e) => {
                 e.stopPropagation();
-                setOpenedContextMenuId(message.messageId);
+                toggleMoreButton();
               }}
               className="size-8 rounded-full flex items-center justify-center hover:bg-white/10"
             >
