b3771cf1178534beed4d8070f6cc1825d24270b6
Ayo100x <ayomideadelooye@gmail.com>
2026-07-25 01:27:02 +0100

    messageTile

--- src/components/messageTile.jsx
+++ src/components/messageTile.jsx
@@ -1,13 +1,25 @@
+import { useState } from "react";
 import MessageBubble from "./messageBubble";
+import { div } from "framer-motion/client";
 
 const MessageTile = ({ activeConversation }) => {
+  const [hoveredMessageId, setHoveredMessageId] = useState(null);
+  const [openedContextMenuId, setOpenedContextMenuId] = useState(null);
+
+  const closeContextMenu = () => {
+    setOpenedContextMenuId(null);
+  }
+
   return (
-    <div className="flex flex-col gap-4">
+    <div onClick={closeContextMenu} className="flex flex-col gap-4">
       {activeConversation.messages.map((message) => {
         const isCurrentUser = message.senderId === 0;
 
         return (
-          <MessageBubble  key={message.messageId} message={message} isCurrentUser={isCurrentUser} />
+          <div key={message.messageId} onMouseEnter={() =>{setHoveredMessageId(message.messageId)}} onMouseLeave={() =>{setHoveredMessageId(null)}}> 
+            <MessageBubble message={message} isCurrentUser={isCurrentUser} setOpenedContextMenuId={setOpenedContextMenuId} hoveredMessageId={hoveredMessageId} openedContextMenuId={openedContextMenuId}  />
+          </div>
+          
         );
       })}
     </div>
