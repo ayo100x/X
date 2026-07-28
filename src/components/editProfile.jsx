bef778c3c54be9318dd3458a5dd18182ea7e75d7
Ayo100x <ayomideadelooye@gmail.com>
2026-07-28 22:40:15 +0100

    Enhance EditProfile component with state management for user inputs

--- src/components/editProfile.jsx
+++ src/components/editProfile.jsx
@@ -1,12 +1,31 @@
 import { X, Sparkles, ChevronRight, Image } from "lucide-react";
+import { useState } from "react";
 
-const EditProfile = ({ user }) => {
+const EditProfile = ({ user, setShowEditProfile }) => {
+  const [name, setName] = useState("");
+  const [bio, setBio] = useState("");
+  const [location, setLocation] = useState("");
+  const [website, setWebsite] = useState("");
+
+  const MAX_BIO = 160;
+
+  const handleBioOnChange = (e) => {
+    if (e.target.value.length <= MAX_BIO) {
+      setBio(e.target.value);
+    }
+  };
+
   return (
     <div className="w-full max-w-170 h-fit max-h-[90vh] flex flex-col bg-[rgb(20,20,20)] text-white rounded-2xl border-[#2f3336] shadow-2xl overflow-hidden m-3 ">
       {/* Header */}
       <div className="flex justify-between items-center px-4 py-3">
         <div className="flex gap-3 text-white items-center">
-          <button className="size-9 rounded-full flex items-center justify-center hover:bg-white/10 transition-colors">
+          <button
+            onClick={() => {
+              setShowEditProfile(false);
+            }}
+            className="size-9 rounded-full flex items-center justify-center hover:bg-white/10 transition-colors"
+          >
             <X size={20} />
           </button>
           <span className="text-[22px] font-semibold tracking-tight text-white">
@@ -17,6 +36,7 @@ const EditProfile = ({ user }) => {
           <span>Save</span>
         </button>
       </div>
+      {/* Body */}
       <div className="overflow-y-auto custom-scrollbar">
         {/* cover photo and avatar */}
         <div className="relative h-52">
@@ -71,19 +91,25 @@ const EditProfile = ({ user }) => {
             <textarea
               rows={1}
               defaultValue=""
+              value={name}
+              onClick={(e) => setName(e.target.value)}
               className="mt-1 w-full resize-none overflow-hidden bg-transparent text-[17px] text-white outline-none"
             />
           </div>
           <div className="m-3 rounded-2xl border border-white/10 px-4 py-3 focus-within:border-[#1d9bf0] transition-colors">
             <div className="flex justify-between">
               <span className="text-[13px] text-white/50">Bio</span>
-              <span className="text-[13px] text-white/50">1/160</span>
+              <span className="text-[13px] text-white/50">
+                {bio.length}/{MAX_BIO}
+              </span>
             </div>
 
             <textarea
               rows={3}
               defaultValue=""
-              className="mt-1 w-full resize-none overflow-hidden bg-transparent text-[17px] text-white outline-none"
+              value={bio}
+              onChange={handleBioOnChange}
+              className="mt-1 w-full resize-none overflow-y-auto custom-scrollbar bg-transparent text-[17px] text-white outline-none"
             />
           </div>
           <div className="m-3 rounded-2xl border border-white/10 px-4 py-3 focus-within:border-[#1d9bf0] transition-colors">
@@ -92,6 +118,8 @@ const EditProfile = ({ user }) => {
             <textarea
               rows={1}
               defaultValue=""
+              value={location}
+              onChange={(e) => setLocation(e.target.value)}
               className="mt-1 w-full resize-none overflow-hidden bg-transparent text-[17px] text-white outline-none"
             />
           </div>
@@ -101,34 +129,35 @@ const EditProfile = ({ user }) => {
             <textarea
               rows={1}
               defaultValue=""
+              value={website}
+              onChange={(e) => setWebsite(e.target.value)}
               className="mt-1 w-full resize-none overflow-hidden bg-transparent text-[17px] text-white outline-none"
             />
           </div>
         </div>
 
         {/* add date of birth */}
-        <div className="px-4 py-3 flex justify-between items-center hover:bg-white/15" >
-            <div className="flex flex-col gap-1">
-              <span className="text-white font-semibold text-[15px]">
-                Birth date
-              </span>
+        <div className="px-4 py-3 flex justify-between items-center hover:bg-white/15">
+          <div className="flex flex-col gap-1">
+            <span className="text-white font-semibold text-[15px]">
+              Birth date
+            </span>
 
-              <span className="text-white/50 text-[14px]">
-                Add your date of birth 
-              </span>
-            </div>
-            <ChevronRight size={25} className="text-white/50" />
+            <span className="text-white/50 text-[14px]">
+              Add your date of birth
+            </span>
+          </div>
+          <ChevronRight size={25} className="text-white/50" />
         </div>
         {/* switch to professional */}
-        <div className="px-4 py-3 flex justify-between items-center hover:bg-white/15" >
-           
-              <span className="text-white font-semibold text-[20px]">
-                Switch to professional
-              </span>
-            
-            <ChevronRight size={25} className="text-white/50" />
+        <div className="px-4 py-3 flex justify-between items-center hover:bg-white/15">
+          <span className="text-white font-semibold text-[20px]">
+            Switch to professional
+          </span>
+
+          <ChevronRight size={25} className="text-white/50" />
         </div>
-        <div className="h-15"/>
+        <div className="h-15" />
       </div>
     </div>
   );
