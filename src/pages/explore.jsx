1318f4c8ef520e842051da222b3a366e582b6cb4
Ayo100x <ayomideadelooye@gmail.com>
2026-08-01 02:49:53 +0100

    Refactor Explore component layout and structure for improved readability

--- src/pages/explore.jsx
+++ src/pages/explore.jsx
@@ -1,10 +1,30 @@
+import ExploreHeader from "../components/ExploreHeader";
+import ExploreTabs from "../components/ExploreTabs";
+import PostsForYou from "../components/postsForYou";
+import TodaysNews from "../components/todaysNews";
+import TrendList from "../components/trendList";
+import WhoToFollow from "../components/whoToFollow";
+
 
 const Explore = () => {
-  return (
-    <div>
-      Explore
-    </div>
-  )
-}
 
-export default Explore
+  
+  return (
+    <div className="h-full relative flex flex-col">
+      <div>
+        <ExploreHeader />
+        <ExploreTabs />
+      </div>
+      {/* body */}
+      <div className="mt-3 overflow-y-auto custom-scrollbar">
+        <TodaysNews />
+        <TrendList />
+        <WhoToFollow />
+        <PostsForYou />
+        
+      </div>
+    </div>
+  );
+};
+
+export default Explore;
