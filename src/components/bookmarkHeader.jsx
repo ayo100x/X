c633c1b2c6caff71607caa2ddec3bc632198718d
Ayo100x <ayomideadelooye@gmail.com>
2026-08-01 04:14:39 +0100

    Enhance BookmarkHeader component with navigation functionality and improved layout

--- src/components/bookmarkHeader.jsx
+++ src/components/bookmarkHeader.jsx
@@ -1,10 +1,16 @@
 import { ArrowLeft } from "lucide-react";
 import React from "react";
+import { useNavigate } from "react-router-dom";
 
 const BookmarkHeader = () => {
+
+  const navigate = useNavigate()
+  
   return (
-    <div className="flex items-center gap-2 ">
-      <button className="size-10 rounded-full flex items-center justify-center hover:bg-white/10 transition-colors">
+    <div className="flex items-center gap-4 m-2 ">
+      <button 
+        onClick={() => {navigate(-1)}}
+        className="size-8 rounded-full flex items-center justify-center hover:bg-white/10 transition-colors">
         <ArrowLeft size={20} className="text-white" />
       </button>
       <span className="text-[20px] font-bold text-white leading-none">Bookmarks</span>
